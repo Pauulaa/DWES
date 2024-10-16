7e5d8e7f304cf02e4db5c184e93021a641cfb5f6
Paula <plopezs23@fpcoruna.afundacion.org>
2024-10-16 13:06:44 +0200

    Ejercicio 2

--- sprint1apache/www/site3/example.php
+++ sprint1apache/www/site3/example.php
@@ -1,3 +1,9 @@
-<?php
-echo "Hola, soy la Bruja Escarlata";
-?>
+<html>
+	<body>
+		<h1>El ganador es ...</h1>
+		<?php
+			$nombre = "Bruja Escarlata";
+			echo $nombre;
+		?>
+	</body>
+</html>
---
+++ sprint1apache/www/site3/example2.php
@@ -0,0 +1,11 @@
+<html>
+	<body>
+		<h1>Página de Bienvenida</h1>
+		<?php
+			function dar_bienvenida($nombre){
+			echo "!Bienvenido/a, " . $nombre . "!";
+			}
+			dar_bienvenida("Lisa Simpson");
+		?>
+	</body>
+</html>
