15ce4246595a779f65db6ee46b535b357e59595c
Paula <plopezs23@fpcoruna.afundacion.org>
2024-11-27 14:01:38 +0100

    El primer ejercicio

--- sprint3django/RestAPI/webserviceapp/views.py
+++ sprint3django/RestAPI/webserviceapp/views.py
@@ -2,21 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-from .models import Tlibros, Tcomentarios
+from .models import Tlibros
+from .models import Tcomentarios
 
 def pagina_de_prueba(request):
 	return HttpResponse("<h1>Hola caracola</h1>");
+# Create your views here.
+
 def devolver_libros(request):
 	lista = Tlibros.objects.all()
 	respuesta_final = []
 	for fila_sql in lista:
 		diccionario = {}
 		diccionario['id'] = fila_sql.id
-		diccionario['titulo'] = fila_sql.nombre
-		diccionario['fecha'] = fila_sql.año_publicacion
+		diccionario['nombre'] = fila_sql.nombre
+		diccionario['año_publicacion'] = fila_sql.año_publicacion
+		diccionario['url_imagen'] = fila_sql.url_imagen
+		diccionario['autor'] = fila_sql.autor
 		respuesta_final.append(diccionario)
 	return JsonResponse(respuesta_final, safe=False)
-# Create your views here.
+
 
 def devolver_libro_por_id(request,id_solicitado):
 	libro=Tlibros.objects.get(id=id_solicitado)
@@ -26,11 +31,16 @@ def devolver_libro_por_id(request,id_solicitado):
 		diccionario={}
 		diccionario['id']=fila_comentario_sql.id
 		diccionario['comentario']=fila_comentario_sql.comentario
+		diccionario['libro']=fila_comentario_sql.libro.nombre
+		if(fila_comentario_sql.usuario is not None):
+			diccionario['usuario']=fila_comentario_sql.usuario
 		lista_comentarios.append(diccionario)
+
 	resultado={
 		'id':libro.id,
-		'titulo':libro.nombre,
-		'fecha':libro.año_publicacion,
+		'nombre':libro.nombre,
+		'año_publicacion':libro.año_publicacion,
+		'autor':libro.autor,
 		'comentarios':lista_comentarios
 	}
 
@@ -40,7 +50,7 @@ def devolver_libro_por_id(request,id_solicitado):
 def guardar_comentario(request, libro_id):
 	if request.method != 'POST':
 		return None
-		
+
 	json_peticion=json.loads(request.body)
 	comentario=Tcomentarios()
 	comentario.comentario=json_peticion['nuevo_comentario']
